09a2ce921adb298aa684bdae021443866345abcb
smairpad <44018959+smairpad@users.noreply.github.com>
2020-01-03 13:04:47 +0530

    Changes to Greet file

--- src/test/java/com/example/TestGreeter.java
+++ src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,9 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+   public void greetShouldIncludeGreetingMessage1() {
+    String someone = "World, Thanks you making me Happy Today";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
